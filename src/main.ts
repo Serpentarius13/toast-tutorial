e3d427bdc3d9b3ad56861ecca9a2512bdaf5ba63
Serpentarius13 <serpentarium13@mail.ru>
2023-05-22 21:51:16 +0300

    toasts

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,12 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import VueClickAway from "vue3-click-away";
 
 import { createPinia } from "pinia";
 
 const pinia = createPinia();
 
-import { PiniaLogger } from "pinia-logger";
-
 const app = createApp(App);
 
-app.use(pinia);
-
-
-
-pinia.use(
-  PiniaLogger({
-    expanded: true,
-    disabled: true,
-  })
-);
-
-app.use(VueClickAway);
-
+app.use(pinia)
 app.mount("#app");
